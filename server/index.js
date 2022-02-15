26fb77102a99eff2a0acc28f1b1a4163f607ba97
Justice Dunn <justicedunn@me.com>
2022-02-15 15:56:17 -0700

    Added basic front-end, posting to back-end, and connected the front and back ends

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
-import express from express;
-import bodyParser from body-parser;
-import mongoose from mongoose;
-import cors from cors;
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extend: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extend: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes)
+
+const CONNECTION_URL = 'mongodb+srv://justice:justice@cluster0.vxd6n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5001;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message));
